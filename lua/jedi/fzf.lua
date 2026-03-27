8402e9efc9422e6cac4fb599251650622c72b567
Mahdi Seyedan <mahdi.se@yahoo.com>
2026-03-27 18:52:08 +0330

    remove utils (lazy_call) function and just use anonymous function

--- lua/jedi/fzf.lua
+++ lua/jedi/fzf.lua
@@ -32,32 +32,102 @@ M.opts = {
 }
 
 M.keys = {
-  { "<leader>f", "<cmd>FzfLua files<CR>", desc = "Find Files", mode = "n" },
-  { "<leader>b", "<cmd>FzfLua buffers<CR>", desc = "Buffers", mode = "n" },
-  { "<leader>g", "<cmd>FzfLua buffers<CR>", desc = "Buffers", mode = "n" },
-  { "<leader>g", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep", mode = "n" },
-  { "<leader>sg", "<cmd>FzfLua grep<CR>", desc = "Grep", mode = "n" },
-  { "<leader>sw", "<cmd>FzfLua grep_cword<CR>", desc = "Grep word", mode = "n" },
-  { "<leader>sW", "<cmd>FzfLua grep_cWORD<CR>", desc = "Grep WORD", mode = "n" },
-  { "<leader>sw", "<cmd>FzfLua grep_visual<CR>", desc = "Grep visual", mode = "n" },
-  { "<leader>st", "<cmd>FzfLua tags_live_grep<CR>", desc = "Tags live grep", mode = "n" },
-  { "<leader>sh", "<cmd>FzfLua helptags<CR>", desc = "Helps", mode = "n" },
-  { "<leader>sm", "<cmd>FzfLua manpages<CR>", desc = "Man Pages", mode = "n" },
-  { "<leader>sd", "<cmd>FzfLua diagnostics_document<CR>", desc = "Diagnostics document", mode = "n" },
-  { "<leader>sk", "<cmd>FzfLua keymaps<CR>", desc = "Keymaps", mode = "n" },
+  {
+    "<leader>f",
+    function()
+      require("fzf-lua").files()
+    end,
+    desc = "Find Files",
+    mode = "n",
+  },
+  {
+    "<leader>b",
+    function()
+      require("fzf-lua").buffers()
+    end,
+    desc = "Buffers",
+    mode = "n",
+  },
+  {
+    "<leader>g",
+    function()
+      require("fzf-lua").live_grep()
+    end,
+    desc = "Live Grep",
+    mode = "n",
+  },
+  {
+    "<leader>sg",
+    function()
+      require("fzf-lua").grep()
+    end,
+    desc = "Grep",
+    mode = "n",
+  },
+  {
+    "<leader>sw",
+    function()
+      require("fzf-lua").grep_cword()
+    end,
+    desc = "Grep word",
+    mode = "n",
+  },
+  {
+    "<leader>sW",
+    function()
+      require("fzf-lua").grep_cWORD()
+    end,
+    desc = "Grep WORD",
+    mode = "n",
+  },
+  {
+    "<leader>sw",
+    function()
+      require("fzf-lua").grep_visual()
+    end,
+    desc = "Grep visual",
+    mode = "v",
+  },
+  {
+    "<leader>st",
+    function()
+      require("fzf-lua").tags_live_grep()
+    end,
+    desc = "Tags live grep",
+    mode = "n",
+  },
+  {
+    "<leader>sh",
+    function()
+      require("fzf-lua").helptags()
+    end,
+    desc = "Helps",
+    mode = "n",
+  },
+  {
+    "<leader>sm",
+    function()
+      require("fzf-lua").manpages()
+    end,
+    desc = "Man Pages",
+    mode = "n",
+  },
+  {
+    "<leader>sd",
+    function()
+      require("fzf-lua").diagnostics_document()
+    end,
+    desc = "Diagnostics document",
+    mode = "n",
+  },
+  {
+    "<leader>sk",
+    function()
+      require("fzf-lua").keymaps()
+    end,
+    desc = "Keymaps",
+    mode = "n",
+  },
 }
 
 return M
-
--- vim.keymap.set("n", "<leader>f", fzf.files, { desc = "Find Files" })
--- vim.keymap.set("n", "<leader>b", fzf.buffers, { desc = "Buffers" })
--- vim.keymap.set("n", "<leader>g", fzf.live_grep, { desc = "Live Grep" })
--- vim.keymap.set("n", "<leader>sg", fzf.grep, { desc = "Grep" })
--- vim.keymap.set("n", "<leader>sw", fzf.grep_cword, { desc = "Grep word" })
--- vim.keymap.set("n", "<leader>sW", fzf.grep_cWORD, { desc = "Grep WORD" })
--- vim.keymap.set("v", "<leader>sw", fzf.grep_visual, { desc = "Grep visual" })
--- vim.keymap.set("n", "<leader>st", fzf.tags_live_grep, { desc = "Tags live grep" })
--- vim.keymap.set("n", "<leader>sh", fzf.helptags, { desc = "Helps" })
--- vim.keymap.set("n", "<leader>sm", fzf.manpages, { desc = "Man Pages" })
--- vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "Diagnostics document" })
--- vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "Keymaps" })
